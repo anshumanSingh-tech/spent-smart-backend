6608cc7703062d8edfa68850cd345024c0e00c7f
anshumanSingh-tech <07anshumansingh10@gmail.com>
2026-04-17 16:02:05 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,6 +8,11 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
+if not DATABASE_URL:
+    DATABASE_URL = "postgresql://postgres:Fearmindkiller@localhost:5432/..."
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
